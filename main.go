2ca6ff9f967924091fade242f270209b255b587e
Wojciech Zmuda <zmuda.w@gmail.com>
2025-05-28 00:30:38 +0200

    complete ptau scenario

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"path/filepath"
 
 	_ "github.com/consensys/gnark"
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/backend/groth16/bn254/mpcsetup"
+	cs "github.com/consensys/gnark/constraint/bn254"
 	"github.com/urfave/cli/v2"
 	_ "github.com/worldcoin/ptau-deserializer/deserialize"
 	deserializer "github.com/worldcoin/ptau-deserializer/deserialize"
@@ -69,16 +73,43 @@ func main() {
 	}
 }
 
-func phase1FromPtau(ptau string, r1cs string, phase2 string) error {
-	// Note: The current implementation does not use r1cs
-	file, err := deserializer.InitPtau(ptau)
+func phase1FromPtau(ptauPath string, r1csPath string, phase2Path string) error {
+	ptau, err := deserializer.ReadPtau(ptauPath)
 	if err != nil {
 		return err
 	}
-	err = deserializer.WritePhase1FromPtauFile(file, phase2)
+
+	phase1, err := deserializer.ConvertPtauToPhase1(ptau)
 	if err != nil {
 		return err
 	}
+
+	r1csReader, err := os.Open(r1csPath)
+	if err != nil {
+		return err
+	}
+	defer func(r1csReader *os.File) {
+		err := r1csReader.Close()
+		if err != nil {
+
+		}
+	}(r1csReader)
+	r1cs := groth16.NewCS(ecc.BN254)
+	_, err = r1cs.ReadFrom(r1csReader)
+	if err != nil {
+		return err
+	}
+
+	phase2, _ := mpcsetup.InitPhase2(r1cs.(*cs.R1CS), &phase1)
+	phase2File, err := os.Create(phase2Path)
+	if err != nil {
+		return err
+	}
+	_, err = phase2.WriteTo(phase2File)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
